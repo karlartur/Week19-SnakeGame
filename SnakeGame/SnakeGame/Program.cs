70743570bc0a12d1a56bb45cd2e21c181be057ee
karlartur <89841872+karlartur@users.noreply.github.com>
2022-01-11 11:19:35 +0200

    Add files via upload

---
+++ SnakeGame/SnakeGame/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace SnakeGame
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int score = 0;
+            
+
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
+
+
+            Point snakeTail = new Point(15, 15, 's');
+            Snake snake = new Snake(snakeTail, 5, Direction.RIGHT);
+            snake.Draw();
+
+            FoodGenerator foodGenerator = new FoodGenerator(80, 25, '$');
+            Point food = foodGenerator.GenerateFood();
+            food.Draw();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.Black;
+            FoodGenerator foodGenerator2 = new FoodGenerator(80, 25, '/');
+            Point food2 = foodGenerator2.GenerateFood();
+            food2.Draw();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+            FoodGenerator foodGenerator3 = new FoodGenerator(80, 25, '*');
+            Point food3 = foodGenerator3.GenerateFood();
+            food3.Draw();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            while (true)
+            {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+                if (snake.Eat(food))
+                {
+                    food = foodGenerator.GenerateFood();
+                    food.Draw();
+                    score--;
+                    Console.Beep();
+                }
+                if (snake.Eat(food2))
+                {
+                    food2 = foodGenerator2.GenerateFood();
+                    food2.Draw();
+                    score++;
+                    Console.Beep();
+                }
+                if (snake.Eat(food3))
+                {
+                    food3 = foodGenerator3.GenerateFood();
+                    food3.Draw();
+                    
+                    Console.Beep();
+                    int xOffset = 25;
+                    int yOffset = 8;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.BackgroundColor = ConsoleColor.White;
+                    Console.SetCursorPosition(xOffset, yOffset++);
+                    WriteText("========================", xOffset, yOffset++);
+                    WriteText("        GAME OVER       ", xOffset + 1, yOffset++);
+                    yOffset++;
+                    WriteText($" You Scored {score} points", xOffset + 2, yOffset++);
+                    WriteText("", xOffset + 1, yOffset++);
+                    WriteText("=========================", xOffset, yOffset++);
+                    string Str_score = Convert.ToString(score);
+                    WriteGameOver(Str_score);
+                    Console.ReadLine();
+                }
+
+                else
+                {
+                    snake.Move();
+                }
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKeys(key.Key);
+                }
+                Thread.Sleep(300);
+
+            }
+            string str_score = Convert.ToString(score);
+            WriteGameOver(str_score);
+            Console.ReadLine();
+        }
+
+
+        public static void WriteGameOver(string score)
+        {
+            int xOffset = 25;
+            int yOffset = 8;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.SetCursorPosition(xOffset, yOffset++);
+            WriteText("========================", xOffset, yOffset++);
+            WriteText("        GAME OVER       ", xOffset + 1, yOffset++);
+            yOffset++;
+            WriteText($" You Scored {score} points", xOffset + 2, yOffset++);
+            WriteText("", xOffset + 1, yOffset++);
+            WriteText("=========================", xOffset, yOffset++);
+            Console.Beep();
+        }
+
+
+        public static void WriteText(string text, int xOffset, int YOffset)
+        {
+            Console.SetCursorPosition(xOffset, YOffset);
+            Console.WriteLine(text);
+        }
+
+
+
+
+    }
+}
---
+++ SnakeGame/SnakeGame/Walls.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeGame
+{
+    class Walls
+    {
+        List<Figure> wallList;
+        public Walls(int mapWidth, int mapHeight)
+        {
+            wallList = new List<Figure>();
+            HorizontallLine top = new HorizontallLine(0, 80, 0, '#');
+            VerticalLine left = new VerticalLine(0, 25, 0, '#');
+            HorizontallLine bottom = new HorizontallLine(0, 80, 25, '$');
+            VerticalLine right = new VerticalLine(0, 25, 80, '$');
+
+
+            VerticalLine obstacle = new VerticalLine(10, 13, 50, '%');
+            VerticalLine obstacle2 = new VerticalLine(5, 5, 30, '%');
+            VerticalLine obstacle3 = new VerticalLine(15, 25, 34, '%');
+            VerticalLine obstacle4 = new VerticalLine(6, 16, 45, '%');
+            VerticalLine obstacle5 = new VerticalLine(28, 3, 40, '%');
+            VerticalLine obstacle6 = new VerticalLine(9, 1, 32, '%');
+
+            wallList.Add(top);
+            wallList.Add(left);
+            wallList.Add(bottom);
+            wallList.Add(right);
+            wallList.Add(obstacle);
+            wallList.Add(obstacle2);
+            wallList.Add(obstacle3);
+            wallList.Add(obstacle4);
+            wallList.Add(obstacle5);
+            wallList.Add(obstacle6);
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+
+        }
+        public void Draw()
+        {
+            foreach (var wall in wallList)
+            {
+                wall.Draw();
+
+            }
+        }
+        public bool IsHit(Figure figure)
+        {
+            foreach (var wall in wallList)
+            {
+                if (wall.IsHit(figure))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
